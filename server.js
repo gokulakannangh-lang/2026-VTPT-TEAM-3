3acd1517f1b7bfd233374741163639554995fc4e
Gokula Kannan <gokulakannangh@gmail.com>
2026-09-23 14:17:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+const app = require('./app');
+const connectDB = async () => {
+  try {
+    const dbConnect = require('./config/db');
+    await dbConnect();
+  } catch (error) {
+    console.error('Failed to connect to the database', error);
+  }
+};
+
+const startServer = async () => {
+  // Connect to database
+  await connectDB();
+
+  const PORT = process.env.PORT || 5000;
+
+  const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+  });
+
+  // Handle unhandled promise rejections
+  process.on('unhandledRejection', (err, promise) => {
+    console.error(`Unhandled Rejection: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+  });
+};
+
+startServer();
